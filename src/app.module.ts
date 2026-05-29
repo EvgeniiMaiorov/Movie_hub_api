f585ccdb5a9cb8cc561d3316bf7da2462d548089
Yauheni Mayorau <evgenii.maiorov.dev@gmail.com>
2026-05-29 19:49:48 +0200

    Add Prisma ORM and PostgreSQL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
