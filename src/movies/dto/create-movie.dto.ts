bbf0cb9613d5bbdf35a4a8d48c39b4bea97661be
Yauheni Mayorau <evgenii.maiorov.dev@gmail.com>
2026-05-29 17:08:23 +0200

    Add validation

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,7 +1,20 @@
+import { IsInt, IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsInt()
   releaseYear: number;
+
+  @IsString()
   genre: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(10)
   rating: number;
 }
