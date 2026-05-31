5603ad917b1560155531b3a7cf7b117e994dcc3f
Yauheni Mayorau <evgenii.maiorov.dev@gmail.com>
2026-05-31 15:21:14 +0200

    Fix prisma client generation and add it to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
-import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
