1b443ea8c2def3aca8fff35608f27f741e6d9f25
Yauheni Mayorau <evgenii.maiorov.dev@gmail.com>
2026-06-01 13:13:40 +0200

    Add swagger documentation

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,19 +1,36 @@
 import { IsInt, IsNumber, IsString, Max, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMovieDto {
+  @ApiProperty({
+    example: 'Interstellar',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+  })
   @IsString()
   description: string;
 
+  @ApiProperty({
+    example: 2014,
+  })
   @IsInt()
   releaseYear: number;
 
+  @ApiProperty({
+    example: 'Sci-Fi',
+  })
   @IsString()
   genre: string;
 
-  @IsNumber()
+  @ApiProperty({
+    example: 8.6,
+  })
+  @IsNumber({ maxDecimalPlaces: 1 })
   @Min(0)
   @Max(10)
   rating: number;
